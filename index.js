7cb44f1ca01ed630079c0d109cfcf97b5bb38853
MIDAROU <mahdibenmhenni0@gmail.com>
2021-12-24 19:17:21 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+//View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+//Send Styles
+app.use(express.static(path.join(__dirname, "/views")));
+//Date Middleware
+app.use(
+	(addActiveTime = (req, res, next) => {
+		let requestAt = new Date().getHours();
+
+		if (requestAt <= 9 || requestAt >= 17) {
+			res.render("NoAcces");
+		} else {
+			next();
+		}
+	})
+);
+//Server Routes
+app.get("/", (req, res) => {
+	res.render("Home");
+});
+app.get("/Services", (req, res) => {
+	res.render("Services");
+});
+app.get("/Contact", (req, res) => {
+	res.render("Contact");
+});
+app.get("*", (req, res) => {
+	res.render("404");
+});
+//Server listen
+app.listen(4000, () => {
+	console.log("Server Running on http://localhost:4000");
+});
